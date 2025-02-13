e1c580fb9c14d44faab21ad53d34a5d505439a8f
yasuaki640 <yasuaki640@gmail.com>
2025-02-13 22:58:22 +0900

    connect db

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/yasuaki640/go-intermediate-playground
 
 go 1.23.4
 
-require github.com/go-chi/chi/v5 v5.2.0 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.0
+	github.com/go-sql-driver/mysql v1.8.1
+)
+
+require filippo.io/edwards25519 v1.1.0 // indirect
--- main.go
+++ main.go
@@ -1,24 +1,25 @@
 package main
 
 import (
-	"github.com/go-chi/chi/v5"
-	"github.com/yasuaki640/go-intermediate-playground/handlers"
-	"log"
-	"net/http"
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	r := chi.NewRouter()
+	dbUser := "docker"
+	dbPassword := "docker"
+	dbDatabase := "sampledb"
+	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
 
-	r.HandleFunc("/", handlers.HelloHandler)
-	r.Post("/article", handlers.PostArticleHandler)
-	r.Get("/article/list", handlers.ArticleListHandler)
-	r.Get("/article/{id}", handlers.ArticleDetailHandler)
-	r.Post("/article/nice", handlers.PostNiceHandler)
-	r.Post("/comment", handlers.PostCommentHandler)
-
-	log.Println("listening at port 8080")
-
-	err := http.ListenAndServe(":8080", r)
-	log.Fatal(err) // exit 1のときにログ出力される
+	if err := db.Ping(); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("connect to DB")
+	}
 }
